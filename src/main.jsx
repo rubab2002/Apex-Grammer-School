aaa850b5e159604c9cb62efee33efbc83de341f1
[200~rubab2002cleargit config --global user.name rubab2002git config --global user.name [200~rubab2002 <rubabashraf497@gmail.com>
2026-05-18 12:44:45 +0500

    Connect GA-4

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-77BMJWPKBB");
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
